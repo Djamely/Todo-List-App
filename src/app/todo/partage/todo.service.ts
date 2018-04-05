116f141e7f887fd18cf071e333f23926b6ebd32b
Djamely <Djamely@users.noreply.github.com>
2018-04-05 18:41:14 +0200

    Add files via upload

---
+++ src/app/todo/partage/todo.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database'
+
+@Injectable()
+export class TodoService {
+  todolist: AngularFireList<any>;
+  constructor(private firebasedb: AngularFireDatabase) { }
+
+
+  getToDoList() {
+    this.todolist = this.firebasedb.list('titles');
+    return this.todolist;
+  }
+
+  addTitle (title: string){
+    this.todolist.push({
+      title: title,
+      isChecked: false
+    });
+  }
+
+  checkOrUncheckTitle ($key: string, flag: boolean) {
+    this.todolist.update($key, {isChecked: flag});
+  }
+
+removeTitle($key: string){
+  this.todolist.remove($key);
+}
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDnIlOuLUEyWrAd2dJyE5jFci6E1oPcq_8",
+    authDomain: "todoliste-a5393.firebaseapp.com",
+    databaseURL: "https://todoliste-a5393.firebaseio.com",
+    projectId: "todoliste-a5393",
+    storageBucket: "todoliste-a5393.appspot.com",
+    messagingSenderId: "998527481538"
+  }
+}
+
